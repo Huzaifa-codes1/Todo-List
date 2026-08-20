14c0775bf940de2a53ef13d0bb98bb162a131b8d
Huzaifa-codes1 <huzaifasarwar222@gmail.com>
2026-08-20 22:28:58 +0500

    Added checkbox and its functionality

--- script.js
+++ script.js
@@ -6,10 +6,30 @@ addbtn.addEventListener("click", () => {
   if (input.value == "") {
   } else {
     let addnewli = document.createElement("li");
+    let inputspan=document.createElement("span")
    const  dltbtn = document.createElement("button")
+   const checkbox=document.createElement("input")
+
+   checkbox.type="checkbox"
         dltbtn.innerText="Delete"
-    addnewli.innerText = input.value;
-addnewli.append(dltbtn)
+
+    inputspan.innerText = input.value;
+
+    addnewli.append(checkbox,inputspan,dltbtn)
+
+     checkbox.addEventListener("click",()=>{
+          
+           if(checkbox.checked){
+            console.log("true")
+            inputspan.style.color="grey"
+            inputspan.style.textDecoration="line-through"
+           }else{
+                        inputspan.style.color="black"
+
+            inputspan.style.textDecoration="none"
+           }
+     })
+
 dltbtn.addEventListener("click",()=>{
 addnewli.remove()
 })
