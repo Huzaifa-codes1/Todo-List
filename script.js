8d35d9cb2dfe04df0b322f7d5631558868ca2939
Huzaifa-codes1 <huzaifasarwar222@gmail.com>
2026-08-21 11:52:36 +0500

    Added edit and save features to Todo-List

--- script.js
+++ script.js
@@ -6,16 +6,44 @@ addbtn.addEventListener("click", () => {
   if (input.value == "") {
   } else {
     let addnewli = document.createElement("li");
+    let editbtn = document.createElement("button")
     let inputspan=document.createElement("span")
    const  dltbtn = document.createElement("button")
    const checkbox=document.createElement("input")
+
+      let editabletask;
+
+
+      editbtn.innerText="Edit"
 
    checkbox.type="checkbox"
         dltbtn.innerText="Delete"
 
     inputspan.innerText = input.value;
 
-    addnewli.append(checkbox,inputspan,dltbtn)
+    addnewli.append(checkbox,inputspan,editbtn,dltbtn)
+
+    editbtn.addEventListener("click",()=>{
+
+      if(editbtn.innerText=="Edit"){
+            editabletask=document.createElement("input")
+
+         editbtn.innerText="save"
+
+      editabletask.value=inputspan.innerText
+     inputspan.replaceWith(editabletask)
+      }
+      else if(editbtn.innerText=="save"){
+                inputspan.innerText=editabletask.value
+                        editabletask.replaceWith(inputspan)
+
+
+        editbtn.innerText="Edit"
+      }
+   
+
+         
+    })
 
      checkbox.addEventListener("click",()=>{
           
